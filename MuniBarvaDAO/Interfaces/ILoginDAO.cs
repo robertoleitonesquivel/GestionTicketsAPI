f8b2e91e4f74e2ff9a228d061bf4cea7913e38c7
robertoleitonesquivel <rleiton22@gmail.com>
2023-09-19 19:42:55 -0600

    inicio proyecto

---
+++ MuniBarvaDAO/Interfaces/ILoginDAO.cs
@@ -0,0 +1,15 @@
+﻿using MuniBarva.MODELS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuniBarva.DAO.Interfaces
+{
+    public interface ILoginDao
+    {
+        Task<Employees> SignIn(string _email, string _password); 
+        Task SaveToken(string _token, string _email);
+    }
+}
